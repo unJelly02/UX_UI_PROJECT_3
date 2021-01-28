dda5cbeea981028b05cd5bbd1e07f8555255e663
Arianahorn <74698341+Arianahorn@users.noreply.github.com>
2021-01-27 17:57:02 -0800

    new style

--- signin.js
+++ signin.js
@@ -9,3 +9,17 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+// eye
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function(e) {
+	const type = password.getAttribute('type')
+=== 'password' ? 'text' : 'password'
+;
+password.setAttribute('type', type);
+this.classList.toggle('fa-eye-slash');
+});
