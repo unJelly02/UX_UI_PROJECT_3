f0e3213bdd7e58638237f685fd65b0ad50315bc5
unJelly02 <32471586+unJelly02@users.noreply.github.com>
2021-01-27 18:14:26 -0600

    Added mainpage

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 $(function(){
     $("#header").load("header.html"); 
     $("#footer").load("footer.html"); 
+    $("#mainPage").load("mainPage.html"); 
 });
 
 // document.getElementById("id").innerHTML='<object type="text/html" data="x.html"></object>';
